8451bbc7da92394885119a7d18de39c527cac897
ansecede <ansecede@espol.edu.ec>
2022-11-25 01:54:45 -0500

    Final changes, app looks good

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -230,9 +230,26 @@ const HomeScreen = () => {
         backgroundColor: '#FFFFFF',
       }}>
       <Modal animationType="slide" transparent={true} visible={modalVisible}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <View>
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <View
+            style={{
+              height: '80%',
+              aspectRatio: 1 / 1.2,
+              margin: 20,
+              backgroundColor: 'white',
+              borderRadius: 20,
+              padding: 35,
+              alignItems: 'center',
+              shadowColor: '#000',
+              shadowOffset: {
+                width: 0,
+                height: 2,
+              },
+              shadowOpacity: 0.25,
+              shadowRadius: 4,
+              elevation: 5,
+            }}>
+            <View style={{ marginBottom: 20 }}>
               <Text
                 style={{
                   color: '#295675',
@@ -247,63 +264,95 @@ const HomeScreen = () => {
               style={{
                 //flex: 1,
                 // justifyContent: "space-between",
-                padding: 10,
                 justifyContent: 'center',
                 alignItems: 'center',
-                width: 300,
-                height: '15%',
-                borderWidth: 2.5,
-                borderColor: '#52ADEB',
-                borderRadius: 10,
-                margin: 20,
-              }}>
-              <Text>{recommendation}</Text>
-            </View>
-            <View
-              style={{
-                width: '80%',
-                height: '42%',
+                width: '100%',
+                aspectRatio: 6 / 1,
                 borderWidth: 2.5,
                 borderStyle: 'solid',
                 borderColor: '#52ADEB',
                 borderRadius: 10,
-                margin: 20,
+                marginTop: 10,
               }}>
+              <Text
+                style={{ fontWeight: 'bold', fontSize: 20, color: 'black' }}>
+                {recommendation}
+              </Text>
+            </View>
+            <View
+              style={{
+                width: '100%',
+                height: 175 / 1,
+                borderWidth: 2.5,
+                borderStyle: 'solid',
+                borderColor: '#52ADEB',
+                borderRadius: 10,
+                marginTop: 30,
+                marginBottom: 30,
+              }}>
+              {/* Flex para poner la ubicacion horizontalmente */}
               <View
                 style={{
                   flexDirection: 'row',
-                  width: '90%',
-                  height: 100,
+                  width: '100%',
+                  height: '50%',
                   justifyContent: 'space-evenly',
                   borderBottomWidth: 2,
                   borderBottomColor: '#52ADEB',
-                  margin: 20,
                 }}>
                 <View
-                  style={{
-                    flex: 0.4,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}>
-                  <Text>Edificio 16A</Text>
+                  style={{ justifyContent: 'center', alignItems: 'center' }}>
+                  <Text style={{ color: 'black' }}>Edificio 16A</Text>
                 </View>
+                {/* Linea divisora */}
                 <View
                   style={{
-                    flex: 0.4,
                     justifyContent: 'center',
-                    alignItems: 'center',
-                  }}>
-                  <Text>Lab de Sistemas Telemáticos</Text>
+                    alignSelf: 'center',
+                    width: 1.5,
+                    height: '70%',
+                    backgroundColor: 'gray',
+                  }}></View>
+                <View style={{ justifyContent: 'center' }}>
+                  <Text style={{ textAlign: 'center', color: 'black' }}>
+                    Lab. de Sistemas Telemáticos
+                  </Text>
                 </View>
               </View>
-              <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{}}>Ahorro Energético esperado</Text>
-                <Text style={{ fontSize: 30, padding: 10, color: '#295675' }}>
+              {/* Ahorro energetico */}
+              <View
+                style={{
+                  width: '100%',
+                  height: '50%',
+                  justifyContent: 'center',
+                }}>
+                <Text
+                  style={{ textAlign: 'center', fontSize: 20, color: 'black' }}>
+                  Ahorro Energético esperado:
+                </Text>
+                <Text
+                  style={{
+                    textAlign: 'center',
+                    fontSize: 30,
+                    padding: 5,
+                    color: '#295675',
+                  }}>
                   {energy_saving}
                 </Text>
               </View>
-              <View style={styles.container}>
-                <View style={styles.buttonContainer}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  marginTop: 15,
+                  width: '100%',
+                  justifyContent: 'space-around',
+                }}>
+                <View
+                  style={{
+                    width: '60%',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
                   <FormButton
                     buttonTitle="Aceptar"
                     onPress={() => {
@@ -313,7 +362,12 @@ const HomeScreen = () => {
                     }}
                   />
                 </View>
-                <View style={styles.buttonContainer}>
+                <View
+                  style={{
+                    width: '60%',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
                   <FormButton
                     buttonTitle="Rechazar"
                     onPress={() => {
@@ -323,6 +377,27 @@ const HomeScreen = () => {
                   />
                 </View>
               </View>
+              {/* <View>
+                <View>
+                  <FormButton
+                    buttonTitle="Aceptar"
+                    // onPress={() => {
+                    //   setControl().then(r => console.log('se envió control'));
+                    //   setModalVisible(!modalVisible);
+                    //   aceptedRecommendation(1);
+                    // }}
+                  />
+                </View>
+                <View>
+                  <FormButton
+                    buttonTitle="Rechazar"
+                    // onPress={() => {
+                    //   setModalVisible(!modalVisible);
+                    //   aceptedRecommendation(0);
+                    // }}
+                  />
+                </View>
+              </View> */}
             </View>
           </View>
         </View>
@@ -582,62 +657,63 @@ const HomeScreen = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '125%',
-    alignSelf: 'flex-start',
-    marginTop: 60,
-  },
-  buttonContainer: {
-    flex: 1,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-  },
-  contentView: {
-    padding: 20,
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'stretch',
-  },
-  button: {
-    marginTop: 10,
-    width: '95%',
-    //height: windowHeight / 5,
-    backgroundColor: '#32A9E9',
-    padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 5,
-  },
-  buttonText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#ffffff',
-    fontFamily: 'Lato-Regular',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     width: '125%',
+//     alignSelf: 'flex-start',
+//     marginTop: 60,
+//   },
+//   buttonContainer: {
+//     width: '55%',
+//     aspectRatio: 2 / 1,
+//   },
+//   modalView: {
+//     margin: 20,
+//     backgroundColor: 'white',
+//     borderRadius: 20,
+//     padding: 35,
+//     alignItems: 'center',
+//     shadowColor: '#000',
+//     shadowOffset: {
+//       width: 0,
+//       height: 2,
+//     },
+//     shadowOpacity: 0.25,
+//     shadowRadius: 4,
+//     elevation: 5,
+//   },
+//   centeredView: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     marginTop: 22,
+//   },
+//   contentView: {
+//     padding: 20,
+//     width: '100%',
+//     justifyContent: 'center',
+//     alignItems: 'stretch',
+//   },
+//   button: {
+//     marginTop: 10,
+//     width: '95%',
+//     //height: windowHeight / 5,
+//     backgroundColor: '#32A9E9',
+//     padding: 10,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     borderRadius: 5,
+//   },
+//   buttonText: {
+//     fontSize: 18,
+//     fontWeight: 'bold',
+//     color: '#ffffff',
+//     fontFamily: 'Lato-Regular',
+//   },
+// });
 
 export default HomeScreen;
